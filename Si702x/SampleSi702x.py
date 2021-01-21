6c6aab99e9b1b120f62513407c6d90b980896fe7
Paul G Crumley <pgcrumley@gmail.com>
2021-01-21 09:14:17 -0500

    add ability to add extra info to ID in output

--- Si702x/SampleSi702x.py
+++ Si702x/SampleSi702x.py
@@ -2,7 +2,7 @@
 """
 MIT License
 
-Copyright (c) 2017 Paul G Crumley
+Copyright (c) 2017, 2020 Paul G Crumley
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -32,6 +32,7 @@ send data in format of:
 
 """
 
+import argparse
 import datetime
 import time
 
@@ -41,7 +42,7 @@ DEBUG = 0
 if DEBUG:
     import sys
 
-SAMPLE_INTERVAL_IN_SECONDS = 60 * 10
+DEFAULT_SAMPLE_INTERVAL_IN_SECONDS = 60 * 10
 OUTPUT_FORMAT = '{} {} {} {:.3f} {:.3f}\n'
 RESULT_FILENAME_BASE = '/opt/Sensors/logs/'
 
@@ -50,14 +51,53 @@ RESULT_FILENAME_BASE = '/opt/Sensors/logs/'
 #
 if __name__ == '__main__':
     
+    parser = argparse.ArgumentParser(description="Capture and log data from a Si702x sensor.")
+    parser.add_argument("-d", "--debug", help="turn on debugging", action="store_true")
+    parser.add_argument("-i", "--interval", help="how often to sample sensors in seconds", 
+                        default=DEFAULT_SAMPLE_INTERVAL_IN_SECONDS)
+    parser.add_argument("-s", "--id_suffix", help="apply a suffix to the id", 
+                        default="")
+    args = parser.parse_args()
+
+    if (args.debug):
+        DEBUG = 1
+        print('turned on DEBUG from command line.',
+              file=sys.stderr, flush=True)
+
+    id_suffix = args.id_suffix
+    sample_interval_in_seconds = int(args.interval)
+    
+    if DEBUG:
+        print('id_suffix = "{}"'.format(id_suffix),
+              file=sys.stderr, flush=True)
+        print('sample_interval_in_seconds = {}'.format(sample_interval_in_seconds),
+              file=sys.stderr, flush=True)
+
+    if sample_interval_in_seconds < 0:
+        sample_interval_in_seconds = 0
+        if DEBUG:
+            print('negative sample interval set to 0',
+                  file=sys.stderr, flush=True)
+    
+    ##
+    ## done parsing command line
+    ##
+
     sensor = Si702x.Si702x()
     chip_type = sensor.get_chip_type()
-    chip_eid = sensor.get_chip_eid()
+    id_ = sensor.get_chip_eid()
     if DEBUG:
-        print('sensor.get_chip_eid() = "{}":"{}"'.format(chip_type, chip_eid),
+        print('sensor.get_chip_eid() = "{}":"{}"'.format(chip_type, id_),
               file=sys.stderr)
 
-    data_name = RESULT_FILENAME_BASE + chip_type + '_' + chip_eid
+    if '' != id_suffix:
+        id_ = id_ + '_' + id_suffix
+    if DEBUG:
+        print('id to use = "{}"'.format(id_),
+              file=sys.stderr)
+
+
+    data_name = RESULT_FILENAME_BASE + chip_type + '_' + id_
     
     with open(data_name, 'a') as output:
         next_sample_time = time.time()
@@ -66,13 +106,13 @@ if __name__ == '__main__':
             temperature,rh = sensor.retrieve_temp_humidity_with_retries()    
             result = OUTPUT_FORMAT.format(when,
                                           chip_type,
-                                          chip_eid, 
+                                          id_, 
                                           temperature,
                                           rh)
             output.write(result)
             output.flush()
             
-            next_sample_time = next_sample_time + SAMPLE_INTERVAL_IN_SECONDS
+            next_sample_time = next_sample_time + sample_interval_in_seconds
             delay_time = next_sample_time - time.time()
             if DEBUG:
                 print('delay_time = {}'.format(delay_time),

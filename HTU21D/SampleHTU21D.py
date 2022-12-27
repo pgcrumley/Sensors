5f134c2deaac422e8c4e4c872ff9ba1736a5b6b8
Paul G Crumley <pgcrumley@gmail.com>
2022-12-27 11:59:27 -0500

    updates for HTU21D sensor

--- HTU21D/SampleHTU21D.py
+++ HTU21D/SampleHTU21D.py
@@ -2,7 +2,7 @@
 """
 MIT License
 
-Copyright (c) 2017, 2019 Paul G Crumley
+Copyright (c) 2017, 2022 Paul G Crumley
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -35,19 +35,19 @@ Capture data from a HTU21D device attached to a Raspberry Pi and
 periodically send the results to a file.
 
 send data in format of:
-  <Timestamp> <Device> <RPI_ID> <degrees C> <Rel Humidity>
+  <Timestamp> <Device> <sensor_id> <degrees C> <Rel Humidity>
 """
 
+import argparse
 import datetime
+import sys
 import time
 
 import HTU21D
 
 DEBUG = 0
-if DEBUG:
-    import sys
 
-SAMPLE_INTERVAL_IN_SECONDS = 60 * 10
+DEFAULT_SAMPLE_INTERVAL_IN_SECONDS = 60 * 10
 OUTPUT_FORMAT = '{} {} {} {:.3f} {:.3f}\n'
 RESULT_FILENAME_BASE = '/opt/Sensors/logs/HTU21D_'
 
@@ -56,36 +56,91 @@ RESULT_FILENAME_BASE = '/opt/Sensors/logs/HTU21D_'
 #
 if __name__ == '__main__':
     
-    # get RPi ID
-    RPI_ID = 'ID_TBD'
-    with open('/proc/cpuinfo') as f:
-        for line in f:
-            info = line.split()
-            if len(info) > 0 and 'Serial' == info[0]:
-                RPI_ID = info[2]
-                break
+    parser = argparse.ArgumentParser(description='Capture and log data from a HTU21D sensor.')
+    parser.add_argument('-d', '--debug',
+                        help='turn on debugging',
+                        action='store_true')
+    parser.add_argument('-i', '--interval',
+                        help='how often to sample sensors in seconds', 
+                        type=int,
+                        default=DEFAULT_SAMPLE_INTERVAL_IN_SECONDS)
+    parser.add_argument('-n', '--name',
+                        help='provide a name rather than an id',
+                        type=str, 
+                        default=None)
+    parser.add_argument('-s', '--id_suffix',
+                        help='apply a suffix to the id',
+                        type=str, 
+                        default=None)
+    args = parser.parse_args()
+
+    if (args.debug):
+        DEBUG = 1
+        print('DEBUG enabled.',
+              file=sys.stderr,
+              flush=True)
+
+    id_suffix = args.id_suffix
+    name = args.name
+    sample_interval_in_seconds = args.interval
     
     if DEBUG:
-        print('RPI_ID = "{}"'.format(RPI_ID), file=sys.stderr)
+        print('sample_interval_in_seconds = {}'.format(sample_interval_in_seconds),
+              file=sys.stderr, flush=True)
+        print('name = "{}"'.format(name),
+              file=sys.stderr, flush=True)
+        print('id_suffix = "{}"'.format(id_suffix),
+              file=sys.stderr, flush=True)
+
+    if sample_interval_in_seconds < 0:
+        sample_interval_in_seconds = DEFAULT_SAMPLE_INTERVAL_IN_SECONDS
+        if DEBUG:
+            print('negative sample interval set to {}'.format(sample_interval_in_seconds),
+                  file=sys.stderr, flush=True)
+
+    ##
+    ## done parsing command line
+    ##
     
-    data_name = RESULT_FILENAME_BASE + RPI_ID
+    # get ID based on Raspberry Pi serial number
+    sensor_id = 'ID_TBD'
+    if name:
+        sensor_id=name
+    else:
+        with open('/proc/cpuinfo') as f:
+            for line in f:
+                info = line.split()
+                if len(info) > 0 and 'Serial' == info[0]:
+                    sensor_id = info[2]
+                    break
+
+    if id_suffix:
+        sensor_id = sensor_id + '_' + id_suffix 
+    
+    if DEBUG:
+        print('sensor_id = "{}"'.format(sensor_id),
+              file=sys.stderr)
+    
+    data_file_name = RESULT_FILENAME_BASE + sensor_id
 
     sensor = HTU21D.HTU21D()
     
-    with open(data_name, 'a') as output:
+    with open(data_file_name, 'a') as output:
+        # start now
         next_sample_time = time.time()
+
         while True:
             when = datetime.datetime.now(datetime.timezone.utc).isoformat()
             temperature,rh = sensor.retrieve_temp_humidity()
             result = OUTPUT_FORMAT.format(when,
                                           sensor.get_chip_type(), 
-                                          RPI_ID,
+                                          sensor_id,
                                           temperature,
                                           rh)
             output.write(result)
             output.flush()
             
-            next_sample_time = next_sample_time + SAMPLE_INTERVAL_IN_SECONDS
+            next_sample_time = next_sample_time + sample_interval_in_seconds
             delay_time = next_sample_time - time.time()
             if DEBUG:
                 print('delay_time = {}'.format(delay_time),
